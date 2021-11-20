53b153925ce718c7cedc1cd02c01af88a18f1638
Max Jöhnk <maxjoehnk@gmail.com>
2021-11-20 20:52:38 +0100

    add angry eyes as indicator to mouse

--- Scenes/Players/Mouse.cs
+++ Scenes/Players/Mouse.cs
@@ -5,11 +5,24 @@ using Godot;
 public class Mouse : Player
 {
     private CheeseThrower Thrower => GetNode<CheeseThrower>("AnimatedSprite/CheeseThrower");
+    private Sprite AngryEyes => GetNode<Sprite>("AngryEyes");
     
     public Mouse() : base(2, 0.0f, 0.0f)
     {
         this.speed = new Vector2(768f, 768f);
         this.IsHunting = false;
+    }
+
+    public override void _Process(float delta)
+    {
+        if (this.IsHunting)
+        {
+            this.AngryEyes.Show();
+        }
+        else
+        {
+            this.AngryEyes.Hide();
+        }
     }
 
     protected override void Attack()
