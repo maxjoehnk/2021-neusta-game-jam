c369aae6e95f0e8a536925caea4cdf5b3a70cb36
Max Jöhnk <maxjoehnk@gmail.com>
2021-11-21 19:15:11 +0100

    Add small timeout before restarting game

--- Scenes/HUD/GameFinishedOverlay.cs
+++ Scenes/HUD/GameFinishedOverlay.cs
@@ -4,10 +4,19 @@ public class GameFinishedOverlay : Node2D
 {
     private Node2D CatWin => GetNode<Node2D>("CatWin");
     private Node2D MouseWin => GetNode<Node2D>("MouseWin");
+    private Timer Timer => GetNode<Timer>("Timer");
+    private Label ContinueText => GetNode<Label>("ContinueText");
 
     [Signal]
     public delegate void RestartGame();
-    
+
+    public override void _Ready()
+    {
+        this.Connect("visibility_changed", this, nameof(this.OnVisibilityChanged));
+        this.Timer.Connect("timeout", this, nameof(this.OnTimeout));
+        this.ContinueText.Hide();
+    }
+
     public void SetWinner(PlayerRole winner)
     {
         if (winner == PlayerRole.Cat)
@@ -24,9 +33,23 @@ public class GameFinishedOverlay : Node2D
 
     public override void _Process(float delta)
     {
-        if (this.Visible && Input.IsActionJustPressed("restart_game"))
+        if (this.Visible && this.Timer.IsStopped() && Input.IsActionJustPressed("restart_game"))
         {
+            this.ContinueText.Hide();
             EmitSignal(nameof(RestartGame));
         }
+    }
+
+    public void OnVisibilityChanged()
+    {
+        if (this.Visible)
+        {
+            this.Timer.Start();
+        }
+    }
+
+    public void OnTimeout()
+    {
+        this.ContinueText.Show();
     }
 }
