a2085a16b32d685e58265452fd69c042a58093e1
Jonathan Kortjohann <jonathan.kortjohann@gmail.com>
2021-11-21 20:55:30 +0100

    Start Screen

--- Scenes/HUD/StartScreen.cs
+++ Scenes/HUD/StartScreen.cs
@@ -6,8 +6,9 @@ public class StartScreen : Node2D
 
 
 
-	private void _on_Button_pressed()
-	{
-		GetTree().ChangeScene("res://Scenes/Game.tscn");
-	}
+private void _on_TextureButton_pressed()
+{
+	GetTree().ChangeScene("res://Scenes/Game.tscn");
+}
+
 }
